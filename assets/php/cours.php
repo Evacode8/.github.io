8dc413f1aeea7234a2e937f066528720249d0ab0
Eva <evegrabocz@hotmail.fr>
2023-10-18 13:46:23 +0200

    modification php

--- assets/php/cours.php
+++ assets/php/cours.php
@@ -1,33 +1,35 @@
+
+
+
 <?php
-// Récupérer les données du formulaire
-$nom = $_POST['fullname'];
-$prenom = $_POST['firstname'];
-$email = $_POST['email'];
-$numero = $_POST['phone'];
+$servername = "localhost";
+$username = "BFS";
+$password = "Perrine_2023";
 
-$ville = $_POST['ville'];
-$projet = $_POST['objectives'];
+try {
+    // Établir la connexion à la base de données
+    $bdd = new PDO("mysql:host=$servername;dbname=cours_bdd", $username, $password);
 
-// Établir la connexion à la base de données
-$mysqli = new mysqli("localhost", "root", "", "cours_bdd");
+    // Définir le mode d'erreur PDO sur Exception
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Vérifier la connexion
-if ($mysqli->connect_error) {
-    die("Erreur de connexion à la base de données : " . $mysqli->connect_error);
+    if(isset($_POST['ok'])){
+        // Récupérer les données du formulaire
+        $nom = $_POST['fullname'];
+        $prenom = $_POST['firstname'];
+        $email = $_POST['email'];
+        $numero = $_POST['phone'];
+        $ville = $_POST['city']; // Assurez-vous que le nom correspond à celui dans votre formulaire
+        $projet = $_POST['objectives']; // Assurez-vous que le nom correspond à celui dans votre formulaire
+
+        // Préparer et exécuter une requête d'insertion
+        $requete = $bdd->prepare("INSERT INTO `cours_clients` (`id_clients`, `Nom_clients`, `Prenom_clients`, `email_clients`, `Numero_clients`, `Ville_client`, `Objectifs_client`) VALUES (NULL, ?, ?, ?, ?, ?, ?)");
+
+        $requete->execute(array($nom, $prenom, $email, $numero, $ville, $projet));
+
+        echo "Inscription réussie !"; // Ou redirigez l'utilisateur vers une page de confirmation
+    }
+} catch (PDOException $e) {
+    echo "Erreur lors de l'inscription : " . $e->getMessage();
 }
-
-// Préparer et exécuter une requête d'insertion
-$sql = "INSERT INTO inscriptions_cours (nom, prenom, ville, projet) VALUES (?, ?, ?, ?)";
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param("ssss", $nom, $prenom, $ville, $projet);
-
-if ($stmt->execute()) {
-    echo "Inscription réussie !"; // Ou redirigez l'utilisateur vers une page de confirmation
-} else {
-    echo "Erreur lors de l'inscription : " . $stmt->error;
-}
-
-// Fermer la connexion
-$stmt->close();
-$mysqli->close();
 ?>
