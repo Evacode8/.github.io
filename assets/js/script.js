690328700cca842a46fed02cab9ab76cc3a36ead
Eva GRABOCZ <egrabocz@leonard-solutions.com>
2023-10-25 17:32:17 +0200

    parralaxe et responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,32 @@
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 40,
 
-  loop: true,
-  centerSlide: "true",
-  fade: "true",
-  grabCursor:"true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 
- 
+// window.addEventListener('scroll', function () {
+//   const scrollValue = window.scrollY;
+//   const image2 = document.querySelector('.image-2');
+
+//   // Ajustez la valeur 2 selon votre besoin pour contrôler la vitesse de l'effet parallax.
+//   image2.style.transform = `translateX(${scrollValue / 2}px)`;
+// });
+
+window.addEventListener('scroll', function () {
+  const scrollValue = window.scrollY;
+  const parallaxTrigger = 900; // Détermine quand arrêter l'effet de parallaxe
+
+
+  if (scrollValue <= parallaxTrigger) {
+    const image1 = document.querySelector('.image-1');
+    const image2 = document.querySelector('.image-2');
+    const parallaxValue = scrollValue / 2;
+  
+  // Appliquez la translation aux deux images en direction opposée
+  image1.style.transform = `translateX(-${parallaxValue}px) rotate(90deg)`;
+  image2.style.transform = `translateX(${parallaxValue}px) rotate(90deg)`;
+  }
+
+
+  
 });
-
-
-
 
 
 
