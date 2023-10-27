83d6567fa8dd7f2a46bfb8b2f4eb80625bb23875
Eva GRABOCZ <egrabocz@leonard-solutions.com>
2023-10-27 13:08:18 +0200

    ajout responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,10 @@
 
 
-
-// window.addEventListener('scroll', function () {
-//   const scrollValue = window.scrollY;
-//   const image2 = document.querySelector('.image-2');
-
-//   // Ajustez la valeur 2 selon votre besoin pour contrôler la vitesse de l'effet parallax.
-//   image2.style.transform = `translateX(${scrollValue / 2}px)`;
-// });
+// evenemtn guitare 
 
 window.addEventListener('scroll', function () {
   const scrollValue = window.scrollY;
-  const parallaxTrigger = 900; // Détermine quand arrêter l'effet de parallaxe
+  const parallaxTrigger = 1200; // Détermine quand arrêter l'effet de parallaxe
 
 
   if (scrollValue <= parallaxTrigger) {
@@ -27,7 +20,6 @@ window.addEventListener('scroll', function () {
 
   
 });
-
 
 
 
